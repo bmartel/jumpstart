da909383f85aa19432e72840c365c32b2ab19f77
Brandon Martel <brandonmartel@gmail.com>
2018-05-04 22:38:43 -0500

    missing a comma

--- template.rb
+++ template.rb
@@ -111,24 +111,24 @@ def add_users
   devise_changes = Dir["db/migrate/**/*devise_changes_to_users.rb"].first
   insert_into_file devise_changes, after: "  def change\n" do
     <<-'RUBY'
-    add_column :users :name, :string, null: false, default: ""
+    add_column :users, :name, :string, null: false, default: ""
 
     ## Confirmable
-    add_column :users :confirmation_token, :string
-    add_column :users :confirmed_at, :datetime
-    add_column :users :confirmation_sent_at, :datetime
-    add_column :users :unconfirmed_email, :string
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string
 
     ## Lockable
-    add_column :users :failed_attempts, :integer, default: 0, null: false
-    add_column :users :unlock_token, :string
-    add_column :users :locked_at, :datetime
+    add_column :users, :failed_attempts, :integer, default: 0, null: false
+    add_column :users, :unlock_token, :string
+    add_column :users, :locked_at, :datetime
 
     ## Announcements
-    add_column :users :announcements_last_read_at, :datetime
+    add_column :users, :announcements_last_read_at, :datetime
 
     ## Admin
-    add_column :users :admin, :boolean, default: false
+    add_column :users, :admin, :boolean, default: false
 
     ## Remove Trackable
     remove_column :users, :sign_in_count,
