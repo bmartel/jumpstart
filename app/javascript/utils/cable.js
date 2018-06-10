577571e92ae1d84dcb84774274d38b718f4a9792
Brandon Martel <brandonmartel@gmail.com>
2018-06-09 22:08:58 -0500

    make all rails js part of webpack and not loaded ever from sprockets

---
+++ app/javascript/utils/cable.js
@@ -0,0 +1,13 @@
+import ActionCable from 'actioncable';
+
+let consumer;
+
+const channel(...args) {
+  if (!consumer) {
+    consumer = ActionCable.createConsumer();
+  }
+
+  return consumer.subscriptions.create(...args);
+}
+
+export default channel;
